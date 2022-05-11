59f344481155019b062b5de2e487bf4903b4e030
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-10 22:02:39 -0300

    ✨ create base component

--- app.vue
+++ app.vue
@@ -1,10 +1,48 @@
 <script lang="ts" setup>
-const { data } = useFetch("/api/todo");
+const { data: todos } = useFetch("/api/todo");
 </script>
 
 <template>
-  <div>
-    {{ data }}
-    <NuxtWelcome />
+  <div class="container">
+    <NCard class="cards">
+      <h1>My Todos</h1>
+      <div class="add-todo">
+        <input placeholder="Add a new todo" />
+        <NButton>Add</NButton>
+      </div>
+      <NCard v-for="todo in todos" class="card" :key="todo.is">
+        <h4>{{ todo.item }}</h4>
+        <p>X</p>
+      </NCard>
+    </NCard>
   </div>
 </template>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  margin: 0 auto;
+  max-width: 50%;
+}
+
+.cards {
+  padding: 0.5rem;
+}
+
+.input {
+  outline: none;
+}
+
+.add-todo {
+  display: flex;
+  justify-content: space-between;
+}
+
+.card {
+  padding: 0.5rem;
+  margin-top: 1rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
