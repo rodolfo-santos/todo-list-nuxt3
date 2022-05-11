32d0b388faa29bae5897760d1645791fc056a9b6
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-10 21:34:17 -0300

    🚚 adding post method

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,8 +1,27 @@
 import { db } from "../db";
+import { v4 as uuid } from "uuid";
 
-export default defineEventHandler((e) => {
+export default defineEventHandler(async (e) => {
   const method = e.req.method;
   console.log(method);
 
-  if (method === "GET") return db.todos;
+  if (method === "GET") {
+    return db.todos;
+  }
+
+  if (method === "POST") {
+    const body = await useBody(e);
+
+    if (!body.item) throw new Error();
+
+    const newTodo = {
+      id: uuid(),
+      item: body.item,
+      completed: false,
+    };
+
+    db.todos.push(newTodo);
+
+    return newTodo;
+  }
 });
