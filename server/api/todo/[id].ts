dbd5d93310566821c2f76b7fadb97f60c574d28f
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-10 21:48:09 -0300

    🚚 adding error handling

--- server/api/todo/[id].ts
+++ server/api/todo/[id].ts
@@ -1,9 +1,33 @@
 import { db } from "../db";
+import { sendError } from "h3";
 
 export default defineEventHandler((e) => {
   const method = e.req.method;
   const context = e.context;
   const { id } = context.params;
+  const findTodoById = (todoId) => {
+    let index = null;
+
+    const todo = db.todos.find((todo, i) => {
+      console.log(todo.id, todoId);
+      if (todo.id === todoId) {
+        index = i;
+        return true;
+      }
+      return false;
+    });
+    if (!todo) {
+      const TodoNotFoundError = createError({
+        statusCode: 404,
+        statusMessage: "Todo not found",
+        data: {},
+      });
+
+      sendError(e, TodoNotFoundError);
+    }
+
+    return { todo, index };
+  };
 
   if (method === "PUT") {
     const { todo, index } = findTodoById(id);
@@ -24,19 +48,3 @@ export default defineEventHandler((e) => {
     return { message: "item deleted" };
   }
 });
-
-function findTodoById(todoId) {
-  let index = null;
-
-  const todo = db.todos.find((todo, i) => {
-    console.log(todo.id, todoId);
-    if (todo.id === todoId) {
-      index = i;
-      return true;
-    }
-    return false;
-  });
-  if (!todo) throw new Error();
-
-  return { todo, index };
-}
